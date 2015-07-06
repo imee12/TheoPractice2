143462fa23d8d5b8f6be8e8a00e0cf6ffd1e236c
Imee Cuison <imee@dynepic.com>
2015-07-06 11:41:05 -0400

    first commit

---
+++ TheoPrac2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheoPrac2
+//
+//  Created by Imee Cuison on 7/6/15.
+//  Copyright (c) 2015 Imee Cuison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
